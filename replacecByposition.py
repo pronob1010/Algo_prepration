c2d891d19f536ba753d8d0bf01a7b295c6bb2022
Pronob <pronobmozumder.info@gmail.com>
2020-09-18 01:30:46 +0600

    not perfect huge bag but working 0.1

--- replacecByposition.py
+++ replacecByposition.py
@@ -3,6 +3,7 @@ class Node():
         self.prev = None
         self.val = value
         self.next = None
+        self.count = 0
 
 class dublyLinkedList():
     def __init__(self):
@@ -12,7 +13,6 @@ class dublyLinkedList():
 
     def add(self, value):
         node = Node(value)
-
         if self.end is None:
             self.start = node
             self.end = node
@@ -29,16 +29,49 @@ class dublyLinkedList():
         temp = self.start
         self.start = node
         self.start.next = temp
+        self.count += 1
 
     def add_last(self,value):
         node = Node(value)
 
         temp = self.end
-
         self.end.next = node
         self.end.prev = temp
-        print(self.end.val)
-        print(self.end.next.val)
+        self.count += 1
+
+    def replaceByPosition(self,pos1,pos2):
+        pos = 0
+        posc = 0
+        temp1 = None
+        temp2 = None
+        current = self.start
+        while current is not None:
+            pos += 1
+            if pos is pos1:
+                temp1 = current
+
+            if pos is pos2:
+                temp2 = current
+            current = current.next
+
+
+
+
+        current2 = self.start
+        while current2 is not None:
+            if posc is pos1:
+                print("temp2 ", temp2.val)
+                current2.val = temp2.val
+                print("cr ", current2.val)
+
+            if posc is pos2:
+                print("temp1 ", temp1.val)
+                current2.val = temp1.val
+                print("cr ", current2.val)
+
+            posc += 1
+            current2 = current2.next
+
 
 
     def __str__(self):
@@ -58,4 +91,12 @@ mylist.add_first(6)
 print(mylist)
 mylist.add_last(7)
 print(mylist)
+print("total element : ", mylist.count)
 
+# pos1, pos2 = list(map(int, input("Enter 2 position that you want to replace : ").split()))
+# print(pos1)
+# print(pos2)
+pos1 =1
+pos2 =2
+mylist.replaceByPosition(pos1,pos2)
+print(mylist)
