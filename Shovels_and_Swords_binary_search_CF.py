dfa72e5f21d0cb3ae60e52166dd11e2a10a4bbc2
Pronob <pronobmozumder.info@gmail.com>
2020-09-18 19:41:05 +0600

    Shovels_and_Swords_binary_search_CF error 1.2

--- Shovels_and_Swords_binary_search_CF.py
+++ Shovels_and_Swords_binary_search_CF.py
@@ -1,27 +1,21 @@
+def result(min, max):
+    if max >= (2 * min):
+        print(min)
+    else:
+        print((min + max) // 3)
+
 n = int(input())
 for i in range(n):
     stick,diamonds = list(map(int, input().split()))
 
-    def result(min,max):
-        if max >= (2*min):
-            print(min)
-        else:
-            print((min + max)//3)
-
-    if stick is diamonds:
+    if stick >= diamonds:
         max = stick
         min = diamonds
-        result(min, max)
-
-    elif stick > diamonds:
-        max = stick
-        min = diamonds
-
-        result(min, max)
 
     elif stick < diamonds:
         max = diamonds
         min = stick
-        result(min, max)
+
+    result(min, max)
 
 
