a0071d1e2e4492f587176e4219827e83cfeb1520
Pronob <pronobmozumder.info@gmail.com>
2020-09-19 15:44:08 +0600

    done

--- cf_1228_A.py
+++ cf_1228_A.py
@@ -1,4 +1,30 @@
-a,b = list(map(int, input().split()))
+def repeated_digit(n):
+    a = []
+    # Traversing through each digit
+    while n != 0:
+        d = n % 10
+        # if the digit is present
+        # more than once in the
+        # number
+        if d in a:
+            # return 0 if the number
+            # has repeated digit
+            return 0
+        a.append(d)
+        n = n // 10
 
-for i in range(a,b):
-    print(i,end=" ")
+    # return 1 if the number has no
+    # repeated digit
+    return 1
+
+l=[]
+a,b = list(map(int, input().split()))
+for i in range(a,b+1):
+    n=i
+    if repeated_digit(n) is 1:
+        l.append(i)
+
+if l is True:
+    print(l[0])
+else:
+    print('-1')
