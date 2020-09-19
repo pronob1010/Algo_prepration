ab5348f812b108bca3d875e2b282c7077e553ba5
Pronob <pronobmozumder.info@gmail.com>
2020-09-20 02:23:51 +0600

    lightoj

---
+++ LO_1001.py
@@ -0,0 +1,21 @@
+t = int(input())
+for i in range(t):
+    n = int(input())
+    a = n//2
+    b = n-a
+    print(a,"",b)
+
+# #include<stdio.h>
+# int main()
+# {
+#     int t,i,a,n,b;
+#     scanf("%d",&t);
+#     for(i = 0;i<t;i++)
+#     {
+#     scanf("%d",&n);
+#     a = n/2;
+#     b = n-a;
+#     printf("%d %d\n",a,b);
+#     }
+#     return 0;
+# }
