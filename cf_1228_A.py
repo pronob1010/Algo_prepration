8d3f61d616ac0ac45039a1a17c5420aace29d5de
Pronob <pronobmozumder.info@gmail.com>
2020-09-20 15:45:12 +0600

    new version added

--- cf_1228_A.py
+++ cf_1228_A.py
@@ -25,3 +25,32 @@ if len(l) > 0:
     print(l[0])
 else:
     print('-1')
+
+
+
+
+#--------------------------------
+
+a,b = list(map(int, input().split()))
+n_li = []
+f = 1
+for i in range(a,b+1):
+    dig = i
+    li = []
+
+    while(dig is not None):
+        if dig%10 in li:
+            f = 0
+            break
+        else:
+            li.append(dig % 10)
+
+        dig = dig//10
+
+    if f is 1:
+        n_li.append(i)
+
+if len(n_li)>0:
+    print(n_li[0])
+else:
+    print('-1')
