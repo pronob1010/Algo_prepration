fe6c5ecaeab6447945a5d8eb2192308248d1e6e4
Pronob <pronobmozumder.info@gmail.com>
2020-09-21 01:31:57 +0600

    finaly done

--- problem_from_coursera.py
+++ problem_from_coursera.py
@@ -1,44 +1,11 @@
-# n = int(input())
-# el = list(map(str,input().strip().split()))[:n]
-#
-# print(el)
-#
-# mx = []
-#
-# for i in len(el):
-#     for j in len(el):
-#         if int(el[i]) is int(el[j]):
-#             continue
-#         else:
-#             mx.append(int(el[i])*int(el[j]))
-#
-# print(max(mx))
-#
-#
-# #unsolveddddddddddddd
+n = int(input())
+el = list(map(int,input().strip().split()))[:n]
 
-a,b = list(map(int, input().split()))
-n_li = []
-f = 1
-for i in range(a,b+1):
-    f = 1
-    dig = i
-    li = []
+mx = []
 
-    while(dig is not None):
-        if dig%10 in li:
-            f = 0
-            break
+for i in range(len(el)):
+    for j in range(len(el)):
+        if not(el[i] == el[j]):
+            mx.append(el[i]*el[j])
 
-        li.append(dig % 10)
-        dig = dig//10
-
-    if f is 1:
-        n_li.append(i)
-
-
-
-if len(n_li)>0:
-    print(n_li[0])
-else:
-    print('-1')
+print(max(mx))
