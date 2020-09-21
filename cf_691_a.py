a9fff88dffa0028d08fee3c9f4b46df908c7b301
Pronob <pronobmozumder.info@gmail.com>
2020-09-21 20:20:49 +0600

    done

--- cf_691_a.py
+++ cf_691_a.py
@@ -1,18 +1,16 @@
 li = []
 n = int(input())
 s = list(map(int,input().strip().split()))[:n]
-li = [int(i) for i in s]
-li.reverse()
+# li = [int(i) for i in s]
+# li.reverse()
 f = 0
 
-for i in range(len(li)):
-    if s[i] is li[i]:
-        f = 0
-    else:
-        f = 1
-        break
+for i in range(len(s)):
+    if s[i] is 1:
+        li.append(s[i])
 
-if f is 0:
+
+if len(li)>1:
     print('YES')
 else:
     print('NO')
