d8d5ad88e99b331c06d0fbd593b3ddb2d8da9025
Pronob <pronobmozumder.info@gmail.com>
2020-09-21 21:59:15 +0600

    done

--- cf_691_a.py
+++ cf_691_a.py
@@ -9,8 +9,14 @@ for i in range(len(s)):
     if s[i] is 1:
         li.append(s[i])
 
-
-if len(li)>1:
-    print('YES')
+if len(li)>0:
+    if len(s)> 1 and (len(li) == len(s)):
+        print('NO')
+    elif len(s) is 1 and (len(li) == len(s)):
+        print('YES')
+    elif len(li) == (len(s)-1):
+        print("YES")
+    else:
+        print('NO')
 else:
     print('NO')
