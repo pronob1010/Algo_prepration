ad1247f096cea9517b75d54d63f00d74f89facf2
Pronob <pronobmozumder.info@gmail.com>
2020-09-22 23:37:44 +0600

    not optimal solution done

--- cf_697_a.py
+++ cf_697_a.py
@@ -1,16 +1,26 @@
-a ,b,c = list(map(int,input().split()))
+a,b,c = list(map(int,input().split()))
+
 p=a
 r =a
 i = 1
-while True:
-    if (c is p) or ( c is r):
-        print("YES")
-        break
 
+while True:
+    if (c == p) or ( c == r):
+        print("YES",i)
+        break
     if r > c or p>c :
         print("No")
         break
-
     p = a + i * b
     r = a + i * b + 1
     i+=1
+
+# def find(p,q,c):
+#     if (c == p) or (c == r):
+#         return 1
+#     if r > c or p>c :
+#         return 0
+#     p = a + i * b
+#     r = a + i * b + 1
+#     find(p, q, c)
+#     find(p, q, c)
