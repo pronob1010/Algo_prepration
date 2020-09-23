409e61f0d3824a72bd5a4ce29bf431fdf6258520
Pronob <pronobmozumder.info@gmail.com>
2020-09-23 12:51:48 +0600

    done

---
+++ leetcode_w1_t1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def singleNumber(self, List):
+        fre = {}
+        for i in List:
+            if i in fre:
+                fre[i] += 1
+            else:
+                fre[i] = 1
+
+        return min(fre, key= lambda x: fre[x])
+
+
+List=[4,1,2,1,2]
+myobj = Solution()
+print(myobj.singleNumber(List))
