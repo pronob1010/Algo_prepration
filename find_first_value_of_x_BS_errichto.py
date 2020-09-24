db6356574e9de59c273d1c622784b066fd27c064
Pronob <pronobmozumder.info@gmail.com>
2020-09-24 18:34:07 +0600

    Algo modification

--- find_first_value_of_x_BS_errichto.py
+++ find_first_value_of_x_BS_errichto.py
@@ -1,6 +1,6 @@
-a = [1,2,3,6,7,8,9]
-x = 4
-store = []
+a = [1,2,3,6,7,9]
+x = 8
+store = 0
 first = 0
 last = len(a)-1
 
@@ -9,11 +9,11 @@ while(first<=last):
     mid = first + (last-first)//2
 
     if a[mid]>=x:
-        store.append(a[mid])
+        store = a[mid]
         last = mid-1
 
     elif a[mid]<x:
         first = mid +1
 
-print(min(store))
+print(store)
 
