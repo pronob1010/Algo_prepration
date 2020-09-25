614811d5975e654db90ead797867ecd59641e4ca
Pronob <pronobmozumder.info@gmail.com>
2020-09-25 17:44:16 +0600

    good to go

--- treeAlgo.py
+++ treeAlgo.py
@@ -8,13 +8,23 @@ class BinarySearchTree():
     def __init__(self, root):
         self.root = Node(root)
 
-    def PreOrderView(self, val):
-        if val
+    def PreOrderView(self, start, string):
+        if start:
+            string += (str(start.value) + "-")
+            string = self.PreOrderView(start.left, string)
+            string = self.PreOrderView(start.right, string)
 
-tree = BinarySearchTree(1)
-tree.left = Node(2)
-tree.left.left = Node(3)
+        return string
+
+
+tree = BinarySearchTree.root(0)
+tree.left = Node(1)
 tree.right = Node(2)
+tree.left.left = Node(3)
+tree.left.right = Node(5)
 tree.right.left = Node(4)
 tree.right.right = Node(5)
-print(tree.right.value)
+
+print(tree.PreOrderView(tree.root, " "))
+print(tree.PreOrderView(tree.left, " "))
+print(tree.PreOrderView(tree.right, " "))
