6a0e4fafa065d7082d0488d9881823aa8cb98029
Pronob <pronobmozumder.info@gmail.com>
2020-09-28 01:41:27 +0600

    Done

--- codingBlocks_Piyush and Magical Park.py
+++ codingBlocks_Piyush and Magical Park.py
@@ -26,27 +26,39 @@
 
 
 n, m , k, s = list(map(int, input().split()))
-f = 0
+res = False
 
 for i in range(n):
     s1 = list(map(str, input().split()))[:m]
-    for j in range(len(s1)):
-        if s1[j] == '#':
-            break
-        elif s1[j] == '*' :
-            s += 5
-            f = 1
-            j+=1
-
-        elif s1[j] == "." :
-            s+=-2
+    if s>=k:
+        for j in range(len(s1)):
+            if s >= k:
+                res = True
+                if s1[j] == '#':
+                    break
+                elif s1[j] == '*':
+                    if j == (len(s1) - 1):
+                        s += 5
+                    else:
+                        s += 4
+                elif s1[j] == ".":
+                    if j == (len(s1) - 1):
+                        s += -2
+                    else:
+                        s += -3
             else:
-                s += -3
-    print("\n",s)
-    f=0
+                res = False
+                break
+    else:
+        res = False
+        break
 
-if s>=k:
+if res == True:
     print("Yes")
     print(s)
 else:
-    print("No")
+    print('No')
+
+
+
+
