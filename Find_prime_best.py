c186d09c8edb69c28b8ee235137451209b2dfc13
Pronob <pronobmozumder.info@gmail.com>
2020-09-29 03:32:38 +0600

    done

--- Find_prime_best.py
+++ Find_prime_best.py
@@ -1,13 +1,31 @@
-import math
-n = int(input())
+# import math
+# n = int(input())
+# r = math.ceil(math.sqrt(n))
+# f =0
+# for i in range(2,r):
+#     if n%i == 0:
+#         f = 1
+#         break
+# if f == 1:
+#     print("No")
+# else:
+#     print("YES, it's Prime")
 
-r = math.ceil(math.sqrt(n))
-f =0
-for i in range(2,r):
-    if n%i == 0:
-        f = 1
-        break
-if f == 1:
-    print("No")
-else:
-    print("YES, it's Prime")
+# import math
+# prime = []
+# def simpleSieve(limit):
+#     mark = [True for i in range(limit + 1)]
+#     p = 2
+#
+#     while (p * p <= limit):
+#         if (mark[p] == True):
+#             for i in range(p * p, limit + 1, p):
+#                 mark[i] = False
+#         p += 1
+#
+#     for p in range(2, limit):
+#         if mark[p]:
+#             prime.append(p)
+#             print(p, end=" ")
+
+# 2 method's
