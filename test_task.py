20f47f0d4fbd3bf77ec26a4a851c74cd02e1dfa4
Pronob <pronobmozumder.info@gmail.com>
2020-09-30 02:20:32 +0600

    done

--- test_task.py
+++ test_task.py
@@ -1,18 +1,27 @@
-# a,b,c = list(map(int, input().split()))
+# # a,b,c = list(map(int, input().split()))
+# #
+# # avg = (a+b+c)/3
+# #
+# # print(avg)
+# #
+# # if 1<= avg <= 20:
 #
-# avg = (a+b+c)/3
+# # a = 5.5454545
+# # b= 5.5454545
+# # print("%.2f %.2f" %(a,b))
 #
-# print(avg)
+# for i in range(8,0,-1):
+#     if (5 & (1 << i)) != 0:
+#         print('1', end="")
 #
-# if 1<= avg <= 20:
+#     else:
+#         print('0', end="")
 
-# a = 5.5454545
-# b= 5.5454545
-# print("%.2f %.2f" %(a,b))
-
-for i in range(8,0,-1):
-    if (5 & (1 << i)) != 0:
-        print('1', end="")
-
-    else:
-        print('0', end="")
+m =input()
+print(type(m))
+print(type(int(m)))
+#
+# if m %2 == 0:
+#     print("Yes")
+# else:
+#     print("NO")
