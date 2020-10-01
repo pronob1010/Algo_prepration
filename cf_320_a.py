e46f007186264a6df6ebc89a67d09b27f17deb82
Pronob <pronobmozumder.info@gmail.com>
2020-10-02 02:00:50 +0600

    done

--- cf_320_a.py
+++ cf_320_a.py
@@ -1,31 +1,48 @@
-n = int(input())
-li = []
-nl1 = []
-while(n!=0):
-    li.append(n%10)
-    n//=10
-li.reverse()
-f = 0
-# nl1.append(li[0])
-t = li[0]
-if li[0] == 1:
-    for i in range(1,len(li)-1):
-        print(li[i])
-        if li[i]!=t:
-            t = li[i]
-            f = 1
-        elif t==1:
-            continue
-        else:
-            f = 0
-            break
-else:
-    f = 0
-
+n = input()
+f = 1
+for i in range(0,len(n)):
+    if n[i-2] == '1' and n[i-1]=='4' and n[i] == '4':
+        i+=3
+    elif n[i-1]=='1' and n[i] == '4':
+        i+=2
+    elif n[i] == '1':
+        i+=1
+    else:
+        f = 0
+        break
 if f == 1:
     print("YES")
 else:
     print("NO")
+
+
+# li = []
+# nl1 = []
+# while(n!=0):
+#     li.append(n%10)
+#     n//=10
+# li.reverse()
+# f = 0
+# # nl1.append(li[0])
+# t = li[0]
+# if li[0] == 1:
+#     for i in range(1,len(li)-1):
+#         print(li[i])
+#         if li[i]!=t:
+#             t = li[i]
+#             f = 1
+#         elif t==1:
+#             continue
+#         else:
+#             f = 0
+#             break
+# else:
+#     f = 0
+#
+# if f == 1:
+#     print("YES")
+# else:
+#     print("NO")
 
 
 
