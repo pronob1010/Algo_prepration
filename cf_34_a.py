4aa3013f7dbaf5f1ecb08b349ae53b6d9de449db
Pronob <pronobmozumder.info@gmail.com>
2020-10-01 15:41:36 +0600

    solved

--- cf_34_a.py
+++ cf_34_a.py
@@ -1,7 +1,18 @@
-
 n = int(input())
 s = list(map(int, input().split()))[:n]
 
+m = 1001
+d = abs(s[0] - s[n-1])
+m = min(d,m)
+idx1 =1
+idx2 =n
+for i in range(1,n):
+    d = abs(s[i-1]-s[i])
+    if m>d:
+        m = d
+        idx1 = (i-1)+1
+        idx2 = i+1
+print(idx1, idx2)
 
 # dic = sorted(([v, i] for i, v in enumerate(s)))
 # print(dic[0][1]+1, dic[1][1]+1)
