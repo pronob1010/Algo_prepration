00c4171fb48a8a0f8f93512ee301d6f8800db61e
Pronob <pronobmozumder.info@gmail.com>
2020-10-01 15:08:55 +0600

    not solved .1

---
+++ cf_34_a.py
@@ -0,0 +1,22 @@
+
+n = int(input())
+s = list(map(int, input().split()))[:n]
+
+
+# dic = sorted(([v, i] for i, v in enumerate(s)))
+# print(dic[0][1]+1, dic[1][1]+1)
+
+# a=[]
+# for i in range(n):
+#     a.insert(s[i],i)
+#
+# print(a)
+#
+# print ("The original list is : " + str(s))
+#
+# res = [((i), s[i]) for i in range(len(s))]
+#
+
+# # printing result
+# print ("The pair list is : " + str(res))
+
