4b80f355a8a30fb5704136cdf09259b3b19a8c9b
Pronob <pronobmozumder.info@gmail.com>
2020-10-03 03:42:35 +0600

    Done

--- cf_466_a.py
+++ cf_466_a.py
@@ -1,14 +1,22 @@
-n,m,a,b = list(map(int,input().split()))
+n, m, a, b = list(map(int, input().split()))
 cost = 0
-u = b/m
+u = b / m
 
-if u<a and n%m == 0:
-    cost+= int(u*n)
+if m>n and (u * m)<(a*n):
+    cost += int(u * m)
 
-elif u<a and n%m == 1:
-    cost+= int(u*n-1)+a
+elif u < a and n % m == 0:
+    cost += int(u * n)
+
+elif u < a and (n % m >=1):
+    if b < a:
+        n += (m - (n % m))
+        cost += int(u * n)
+
+    else:
+        cost += int((u * (n - (n % m))) + (a*(n % m)))
 
 else:
-    cost+= a*n
+    cost += a * n
 
 print(cost)
