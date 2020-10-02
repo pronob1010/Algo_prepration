62faaf122a82b1cc8fb2dfd814d50a16f2444754
Pronob <pronobmozumder.info@gmail.com>
2020-10-02 23:52:59 +0600

    With bit manipulation (optimized)

--- n is a power of 2.py
+++ n is a power of 2.py
@@ -1,11 +1,20 @@
 n = int(input())
 
-if n==0:
-    print("No")
+#normal way
+# if n==0:
+#     print("No")
+# else:
+#     while(n%2==0):
+#         n/=2
+#     if n== 1:
+#         print("Yes")
+#     else:
+#         print("NO")
+
+#optimized way
+
+r = n&(n-1)
+if r== 0:
+    print("Yes")
 else:
-    while(n%2==0):
-        n/=2
-    if n== 1:
-        print("Yes")
-    else:
-        print("NO")
+    print("NO")
