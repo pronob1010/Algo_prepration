56d78c0b7b229fc459472ba88f0e5bb9332b51db
Pronob <pronobmozumder.info@gmail.com>
2020-10-02 23:05:23 +0600

    solved

--- cf_1016_a.py
+++ cf_1016_a.py
@@ -1,10 +1,7 @@
 n,m = list(map(int,input().split()))
 n1= list(map(int,input().split()))[:n]
 c =0
-w = 0
 for i in n1:
-    if i<=m:
-        w = m-i
-    else:
-        c+=1
-        w+=i
+    ans = (c+i)//m
+    c = (c+i)%m
+    print(ans)
