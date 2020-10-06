970328d0273c1284ed06916574cc387832f73958
Pronob <pronobmozumder.info@gmail.com>
2020-10-07 00:18:57 +0600

    solved

--- cf_255_a.py
+++ cf_255_a.py
@@ -1,22 +1,10 @@
-t = int(input())
-a = list(map(int, input().split()))[:t]
+L = [0, 0, 0]
 
-m = max(a)
-for i in range(t):
-    if a[i] == m:
-        break
-if i<=3:
-    if (i + 1) == 1:
-        print("chest")
-    elif (i + 1)== 2:
-        print("biceps")
-    elif (i + 1)== 3:
-        print("back")
-else:
-    if (i + 1) // (3) == 1:
-        print("chest")
-    elif (i + 1) // (3) == 2:
-        print("biceps")
-    elif (i + 1) // (3) == 3:
-        print("back")
+L1 = ["chest", "biceps", "back"]
 
+input()
+
+for k, j in enumerate(map(int, input().split(" "))):
+    L[k % 3] += j
+
+print(L1[L.index(max(L))])
