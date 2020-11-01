be7b055ee2993f0814e582fc9106dfb885164773
Pronob <pronobmozumder.info@gmail.com>
2020-11-02 03:26:52 +0600

    done

--- recursionTest.py
+++ recursionTest.py
@@ -1,13 +1,23 @@
-def fun(x):
-    if (x > 0):
-        x -= 1
-        fun(x)
-        print(x, end=" ")
-        x -= 1
-        fun(x)
+# #test -1
+# def fun(x):
+#     if (x > 0):
+#         x -= 1
+#         fun(x)
+#         print(x, end=" ")
+#         x -= 1
+#         fun(x)
+#
+# a = 4
+# fun(a)
+#
+##test -2
 
-a = 4
-fun(a)
-
-
-
+def fun(n):
+    if n%2==0:
+        n+=1
+        return n
+    else:
+        return fun(fun(n-1))
+if __name__ == '__main__':
+    n = 200
+    print(fun(n))
