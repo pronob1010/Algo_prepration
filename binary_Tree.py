bdd733dd027ef5b8fff8bd0696652cd38cfd6ff6
Pronob <pronobmozumder.info@gmail.com>
2021-03-11 12:41:56 +0600

    insertion added(without order target)

--- binary_Tree.py
+++ binary_Tree.py
@@ -4,8 +4,32 @@ class Node:
         self.right = None
         self.item = value
 
-    def printTree(self):
-        print(self.item)
+    def insert(self, value):
+        if self.item:
+            if value< self.item:
+                if self.left is None:
+                    self.left = Node(value)
+                else:
+                    self.right.insert(value)
 
-root = Node(10)
+            elif value>self.item:
+                if self.right is None:
+                    self.right = Node(value)
+                else:
+                    self.right.insert(value)
+
+    def printTree(self):
+        if self.left:
+            self.left.printTree()
+        print(self.item)
+        if self.right:
+            self.right.printTree()
+
+
+root = Node(12)
+root.insert(6)
+root.insert(14)
+root.insert(3)
+
+
 root.printTree()
