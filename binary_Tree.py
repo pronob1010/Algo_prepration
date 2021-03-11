7be009915abf6fb38ec5bab207064d1e1e8d2a31
Pronob <pronobmozumder.info@gmail.com>
2021-03-12 02:28:12 +0600

    min max added with o(1)

--- binary_Tree.py
+++ binary_Tree.py
@@ -3,16 +3,20 @@ class Node:
         self.left = None
         self.right = None
         self.item = value
+        self.min = value
+        self.max = value
 
     def insert(self, value):
         if self.item:
-            if value< self.item:
+            if value<self.item:
+                self.min = value
                 if self.left is None:
                     self.left = Node(value)
                 else:
                     self.left.insert(value)
 
             elif value>self.item:
+                self.max = value
                 if self.right is None:
                     self.right = Node(value)
                 else:
@@ -43,9 +47,11 @@ class Node:
 root = Node(12)
 root.insert(6)
 root.insert(14)
-root.insert(5)
+root.insert(3)
 
 root.printTree()
 x = root.search(15)
 if x == 0:
     print("Not Found")
+print(root.min)
+print(root.max)
